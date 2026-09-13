f360b36dee9763234926ace4ce36a7ee109c296f
nina19-dot <ninadov19@gmail.com>
2026-09-12 18:16:11 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,49 @@
+# config.py
+
+FEATURES = [
+    'v',
+    'GTT',
+    'GTn',
+    'GGn',
+    'Ts',
+    'T48',
+    'T2',
+    'P48',
+    'P2',
+    'Pexh',
+    'TIC',
+    'mf'
+]
+
+SENSORES = [
+    'GTT',
+    'GTn',
+    'GGn',
+    'Ts',
+    'T48',
+    'T2',
+    'P48',
+    'P2',
+    'Pexh',
+    'TIC',
+    'mf'
+]
+
+VELOCIDADES = [
+    3, 6, 9, 12, 15, 18, 21, 24, 27
+]
+
+UNIDADES = {
+    'v': 'knots',
+    'GTT': 'kN m',
+    'GTn': 'rpm',
+    'GGn': 'rpm',
+    'Ts': 'kN',
+    'T48': '°C',
+    'T2': '°C',
+    'P48': 'bar',
+    'P2': 'bar',
+    'Pexh': 'bar',
+    'TIC': '%',
+    'mf': 'kg/s'
+}
